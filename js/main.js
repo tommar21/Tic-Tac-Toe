68e867c9b82d28c71491d88acd26aec928af21bb
tovadesign <tomimartinez6666@gmail.com>
2020-06-19 19:35:24 -0300

    new update

--- js/main.js
+++ js/main.js
@@ -1,26 +1,39 @@
 const celdas = document.getElementsByClassName('celdas');
+const equis = document.getElementsByClassName('equis');
+const circle = document.getElementsByClassName('circle');
+const xoxo = [
+    [equis[0], equis[1], circle[0]],
+    [equis[2], equis[3], circle[1]],
+    [equis[4], equis[5], circle[2]],
+    [equis[6], equis[7], circle[3]],
+    [equis[8], equis[9], circle[4]],
+    [equis[10], equis[11], circle[5]],
+    [equis[12], equis[13], circle[6]],
+    [equis[14], equis[15], circle[7]],
+    [equis[16], equis[17], circle[8]]
+];
 var ganador = null;
 var turno = 1;
 var quien = document.getElementById('quien');
-var titulo = document.getElementById('turno')
-
+var titulo = document.getElementById('turno');
 
 for (let i = 0; i < celdas.length; i++) {
 
-    celdas[i].addEventListener('click', function() {
+    celdas[i].addEventListener('click', function () {
 
         //compruebo si el cuadrado esta vacio o no
 
-        if (celdas[i].textContent == "" && ganador == null) {
+        if (celdas[i].id == "" && ganador == null) {
 
             //Si son turnos impares poner X
 
             if (turno == 1 || turno == 3 || turno == 5 || turno == 7 || turno == 9) {
 
-                celdas[i].innerHTML = 'X'
-                celdas[i].style.color = 'red'
+                celdas[i].id = 'X'
+                xoxo[i][0].style.strokeDashoffset = '0'
+                xoxo[i][1].style.strokeDashoffset = '0'
                 quien.innerHTML = 'O'
-                quien.style.color = 'blue'
+                quien.style.color = '#f000ff'
                 turno++
 
                 ganador = detganador('X')
@@ -30,10 +43,10 @@ for (let i = 0; i < celdas.length; i++) {
 
             else if (turno == 2 || turno == 4 || turno == 6 || turno == 8) {
 
-                celdas[i].innerHTML = 'O'
-                celdas[i].style.color = 'blue'
+                celdas[i].id = 'O'
+                xoxo[i][2].style.strokeDashoffset = '0'
                 quien.innerHTML = 'X'
-                quien.style.color = 'red'
+                quien.style.color = '#4deeea'
                 turno++
 
                 //Si ya ganó el O ponerlo de ganador
@@ -47,9 +60,9 @@ for (let i = 0; i < celdas.length; i++) {
         if (ganador) {
 
             titulo.innerHTML = 'El ganador es: ' + ganador
-        } 
+        }
         else if (!ganador && turno == 10) {
-            
+
             titulo.innerHTML = 'Empate'
         }
 
@@ -59,14 +72,14 @@ for (let i = 0; i < celdas.length; i++) {
 function detganador(letra) {
 
     if (
-        (celdas[0].innerHTML == letra && celdas[1].innerHTML == letra && celdas[2].innerHTML == letra) ||
-        (celdas[3].innerHTML == letra && celdas[4].innerHTML == letra && celdas[5].innerHTML == letra) ||
-        (celdas[6].innerHTML == letra && celdas[7].innerHTML == letra && celdas[8].innerHTML == letra) ||
-        (celdas[0].innerHTML == letra && celdas[3].innerHTML == letra && celdas[6].innerHTML == letra) ||
-        (celdas[1].innerHTML == letra && celdas[4].innerHTML == letra && celdas[7].innerHTML == letra) ||
-        (celdas[2].innerHTML == letra && celdas[5].innerHTML == letra && celdas[8].innerHTML == letra) ||
-        (celdas[0].innerHTML == letra && celdas[4].innerHTML == letra && celdas[8].innerHTML == letra) ||
-        (celdas[2].innerHTML == letra && celdas[4].innerHTML == letra && celdas[6].innerHTML == letra)
+        (celdas[0].id == letra && celdas[1].id == letra && celdas[2].id == letra) ||
+        (celdas[3].id == letra && celdas[4].id == letra && celdas[5].id == letra) ||
+        (celdas[6].id == letra && celdas[7].id == letra && celdas[8].id == letra) ||
+        (celdas[0].id == letra && celdas[3].id == letra && celdas[6].id == letra) ||
+        (celdas[1].id == letra && celdas[4].id == letra && celdas[7].id == letra) ||
+        (celdas[2].id == letra && celdas[5].id == letra && celdas[8].id == letra) ||
+        (celdas[0].id == letra && celdas[4].id == letra && celdas[8].id == letra) ||
+        (celdas[2].id == letra && celdas[4].id == letra && celdas[6].id == letra)
     ) {
         return letra
     }
@@ -74,3 +87,4 @@ function detganador(letra) {
         return null
     }
 }
+
